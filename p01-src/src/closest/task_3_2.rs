fdc02a490b1757a47963f80e948c312f36fe96c1
Anson <git@anson-cheung.com>
2021-03-01 03:52:20 +0800

    Add y-points

--- p01-src/src/closest/task_3_2.rs
+++ p01-src/src/closest/task_3_2.rs
@@ -1,5 +1,6 @@
 use crate::closest::{find_minimum_bruteforce, find_minimum_in_strip};
 use crate::geometry::{Point, PointPair};
+use itertools::partition;
 
 #[derive(Clone, Debug)]
 pub struct Task3SortedY {
@@ -13,45 +14,54 @@ impl Task3SortedY {
 
     // Entry function
     pub fn find_closest_pair(&mut self) -> PointPair {
+        let mut points_y = self.points.clone();
+        points_y.sort_unstable_by(|a, b| a.y.partial_cmp(&b.y).unwrap());
         self.points
             .sort_unstable_by(|a, b| a.x.partial_cmp(&b.x).unwrap());
-        find_closest_pair_inner(&mut self.points)
+        find_closest_pair_inner(&mut self.points, &mut points_y)
     }
 }
 
-fn find_closest_pair_inner(points: &mut [Point]) -> PointPair {
+fn find_closest_pair_inner(points_x: &mut [Point], points_y: &mut [Point]) -> PointPair {
     // Reference: W2 L3
 
     // Base case: we can't recurse any further
-    if points.len() <= 3 {
-        points.sort_unstable_by(|a, b| a.y.partial_cmp(&b.y).unwrap());
-        return find_minimum_bruteforce(points.iter());
+    if points_x.len() <= 3 {
+        points_x.sort_unstable_by(|a, b| a.y.partial_cmp(&b.y).unwrap());
+        return find_minimum_bruteforce(points_x.iter());
     }
 
     // We have already sorted the entire array
-    let length = points.len();
-    let (left, right) = points.split_at_mut(length / 2);
-    let median = *right.first().unwrap();
+    let length = points_x.len();
+    let (left_x, right_x) = points_x.split_at_mut(length / 2);
+    let median = *right_x.first().unwrap();
+
+    // Split
+    let (left_y, right_y) = {
+        let pt = partition(points_y.iter_mut(), |p| p.x <= median.x);
+        points_y.split_at_mut(pt)
+    };
 
     // Recursively solve the problem by left and right
-    let left_minimum = find_closest_pair_inner(left);
+    let left_minimum = find_closest_pair_inner(left_x, left_y);
 
-    let right_minimum = find_closest_pair_inner(right);
+    let right_minimum = find_closest_pair_inner(right_x, right_y);
 
     // Get minimum distance
     let minimum = left_minimum.min(right_minimum);
 
     // Merge
-    let mut points_cpy = points.to_vec();
+    let mut points_cpy = points_y.to_vec();
+    let length_y = points_cpy.len();
     merge(
-        &points[0..(length / 2)],
-        &points[(length / 2)..],
+        &points_y[0..(length_y / 2)],
+        &points_y[(length_y / 2)..],
         &mut points_cpy,
     );
-    points.copy_from_slice(&points_cpy);
+    points_y.copy_from_slice(&points_cpy);
 
     // Now filter
-    let strip = points
+    let strip = points_y
         .iter()
         .filter(|p| (p.x - median.x).abs() < minimum.distance().0);
 
